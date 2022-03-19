d27e6b4217d58213405e47a3c963b418333ba596
PangHae <yds05074@gmail.com>
2022-03-19 18:15:57 +0900

    delete unusable files, add image sending method

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import {React, useState} from 'react';
+import FormData from 'form-data';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () =>{
+  const [image, setImage] = useState();
+  const formData = new FormData();
+
+  const handleChange = (e) =>{
+    if(e.target.files){
+      setImage(e.target.files[0]);
+      formData.append('files', image);
+    }
+  }
+
+  const sendImage = () =>{
+    console.log('send Image to server!');
+
+    axios({
+      method:'post',
+      url:'http://localhost:5000/api/image-upload',
+      data: formData,
+      headers:{
+        'Content-Type': 'multipart/form-data',
+      }
+    })
+    .then((res)=>{
+      console.log('image send Success!');
+      console.log(`res : ${res}`);
+    })
+    .catch((err) =>{
+      console.log('image send Failed!')
+      console.log(`err : ${err}`);
+    });
+  }
+
+  return(
+    <div>
+      <input type='file' name='file' accept='image/*' onChange={handleChange}/>
+      <button type='button' onClick={sendImage}>
+        Send Image
+      </button>
     </div>
   );
-}
+};
 
 export default App;
