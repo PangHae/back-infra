f0a94304314e9168fdcdc1d3c480de7367830b46
PangHae <yds05074@gmail.com>
2022-04-02 15:40:23 +0900

    add dnd to image

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,24 @@
-import {React, useState} from 'react';
+import {React, useEffect, useState} from 'react';
 import FormData from 'form-data';
 import axios from 'axios';
+import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
 
 const App = () =>{
   const [imagePath, setImagePath] = useState('');
+  const [imageList, setImageList] = useState([]);
+
+  useEffect(() =>{
+      axios('http://localhost:5001/api/get-image-list')
+      .then((res)=>{
+        if(res.data.success){
+          setImageList(res.data.files);
+        }
+      })
+      .catch((err) =>{
+        console.log("Failed to get Imagelist");
+        console.log(err);
+      });
+  }, [imagePath]);
 
   const handleChange = (e) =>{
     if(e.target.files){
@@ -31,17 +46,67 @@ const App = () =>{
     }
   }
 
+  useEffect(()=>{
+
+  },[imageList]);
+
+  const handleOnDragEnd = (result) =>{
+    if(!result.destination){
+      return;
+    }
+    const items = [...imageList];
+    const [recordedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, recordedItem);
+    setImageList(items);
+  };
+
+  const showImageList =(
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="images" direction="horizontal">
+        {(provided) =>(
+          <div 
+            className='images'
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {
+              imageList.map((imageName, id, index) =>{
+                console.log(index[id]);
+                console.log(id)
+                  return (
+                  <Draggable key={index[id]} draggableId={`${id}`} index={id}>
+                    {
+                      (provided) =>(
+                        <img
+                          src={`http://localhost:5001/images/${imageName}`} 
+                          alt='서버 이미지'
+                          width='230px'
+                          height='300px'
+                          ref={provided.innerRef}
+                          {...provided.dragHandleProps}
+                          {...provided.draggableProps}
+                        />
+                      )
+                    }
+                    </Draggable>
+                  );
+                }
+              )
+            }
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable> 
+    </DragDropContext>
+  );
+
   return(
     <>
+      {showImageList}
       <div>
         <input type='file' name='file' accept='image/*' onChange={handleChange}/>
       </div>
-        {
-          imagePath === '' ? null
-            : <div>
-                <img src={`http://localhost:5001/${imagePath}`} alt='서버 이미지'/>
-              </div>
-        }
+        
     </>
   );
 };
